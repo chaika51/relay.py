6f661adb4e3762c94bc4ca1813089ded2175c730
chaika51 <robloxofficial400@gmail.com>
2026-01-09 06:26:12 +0100

    bot.py

---
+++ bot.py
@@ -0,0 +1,18 @@
+import discord
+from discord.ext import commands
+import os
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"✅ Бот запущен как {bot.user}")
+
+@bot.command()
+async def тест(ctx):
+    await ctx.send("✅ Бот работает! Хостинг ок.")
+
+bot.run(os.getenv("MTQxNjc1OTE1Njg4MjI4MDU5OQ.GcqZfM.-7JbMu5Xpw6PAljQXwBYQMTL_RCCiiA23wcG-k"))
